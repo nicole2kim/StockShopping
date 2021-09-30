79ff2523afcf2c71173ddd0a7736e8d8ae9023d7
mongus <kjyanus@gmail.com>
2021-09-30 17:49:29 +0900

    front back

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,17 +59,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'stock',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000']
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'backend.urls'
 
